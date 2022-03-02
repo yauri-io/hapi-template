25431d314bdfa4a53f5595d7408c609870123fed
y-io <me@yauri.one>
2022-03-03 03:14:59 +0800

    fix hapi pino issue

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ const manifest = {
   register: {
     plugins: [
       {
-        plugin: 'hapi-pino', options: {
-          prettyPrint: Config.environment !== 'prod',
+        plugin: 'hapi-pino',
+        options: {
+          transport: {
+            target: "pino-pretty",
+            options: {
+              colorize: true,
+              minimumLevel: "info",
+              levelFirst: true,
+              messageFormat: true,
+              timestampKey: "time",
+              translateTime: true,
+              singleLine: false,
+              mkdir: true,
+              append: true,
+            },
+          },
           logPayload: true,
           level: Config.server.logLevel,
           redact: ['req.headers.authorization'], // do not log authorization in the headers
